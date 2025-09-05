372310f53443143b852beca5912e312e3db33352
Hugo U. R. Strand <hugo.strand@gmail.com>
2025-09-05 12:48:21 +0200

    [clair] draft wrapping of impurity class WIP

---
+++ python/triqs_soehyb/impurity.cpp
@@ -0,0 +1,7 @@
+// clair-c2py --gen-default-config impurity.cpp  --> impurity.toml (template)
+
+#include <c2py/c2py.hpp>
+
+#include "triqs_soehyb/impurity.hpp"
+#include "triqs_soehyb/dlr_dyson_ppsc.hpp"
+
