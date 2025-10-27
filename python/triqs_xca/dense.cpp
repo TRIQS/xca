8b38a5cd4da6b7988f82e8f10bb2285d268592d0
Nils Wentzell <nwentzell@flatironinstitute.org>
2025-10-27 18:05:29 -0400

    Translate missing dense_desc python module to clair/c2py

---
+++ python/triqs_xca/dense.cpp
@@ -0,0 +1,10 @@
+// clair-c2py --gen-default-config dense.cpp  --> dense.toml (template)
+
+#include <c2py/c2py.hpp>
+
+#include <cppdlr/cppdlr.hpp>
+#include "triqs_xca/block_sparse_manual.hpp"
+
+#include "pycppdlr.wrap.hxx"
+
+#include "dense.wrap.cxx"
