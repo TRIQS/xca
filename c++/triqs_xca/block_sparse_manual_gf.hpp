1b6674b004f193fed0a65897ad34fe0c2a2d49f5
Paco Rilloraza <fpr2017@nyu.edu>
2026-08-06 12:35:17 -0400

    [test] third-order Green's f'n trapezoidal comparisons

--- c++/triqs_xca/block_sparse_manual_gf.hpp
+++ c++/triqs_xca/block_sparse_manual_gf.hpp
@@ -30,6 +30,27 @@ nda::array<dcomplex, 3> OCA_gf_tpz(nda::array_const_view<dcomplex, 3> hyb_coeffs
                                    nda::array_const_view<dcomplex, 3> Fs, int n_quad);
 
 /**
+ * @brief Evaluate the third-order single-particle Green's function directly using trapezoidal quadrature
+ *
+ * @details Topology {{0,3},{1,4},{2,5}}, i.e. the integral of
+ * examples/one_fermion_analytical_solutions.ipynb, "Third order single particle Green's function diagram".
+ * Stands to OCA_gf_tpz as third_order_tpz (block_sparse_manual.hpp) stands to OCA_tpz. Returns the result
+ * on the equispaced grid of n_quad + 1 points; both grid endpoints are left at zero.
+ *
+ * @param[in] hyb_coeffs DLR coefficients of the hybridization
+ * @param[in] hyb_refl_coeffs DLR coefficients of the hybridization at negative imaginary times
+ * @param[in] itops cppdlr imaginary time object
+ * @param[in] beta inverse temperature
+ * @param[in] Gt_coeffs DLR coefficients of the pseudoparticle Green's function
+ * @param[in] Fs annihilation operators
+ * @param[in] n_quad number of quadrature intervals
+ * @return third-order contribution to the single-particle Green's function
+ */
+nda::array<dcomplex, 3> third_order_gf_tpz(nda::array_const_view<dcomplex, 3> hyb_coeffs, nda::array_const_view<dcomplex, 3> hyb_refl_coeffs,
+                                           imtime_ops &itops, double beta, nda::array_const_view<dcomplex, 3> Gt_coeffs,
+                                           nda::array_const_view<dcomplex, 3> Fs, int n_quad);
+
+/**
  * @brief Evaluate OCA Green's function using dense storage
  * @param[in] hyb_coeffs hybridization coefficients
  * @param[in] hyb_refl_coeffs hybridization coefficients at negative imag. times
